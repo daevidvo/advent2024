f72250707cb5b6658c867a8dbcd44369d6fc9c8f
David Vo <user@davd-book.local>
2024-12-01 11:05:18 -0800

    added function to add left and right elements into respective arrays

--- d1/main.py
+++ d1/main.py
@@ -1,10 +1,21 @@
 #importing data
-file = open("./d1/d1data.txt","r")
-print(file.read())
+data = open("./d1/d1data.txt","r").read().splitlines()
 
-
-
-
+#split columns into two arrays
+#sort arrays
+#go through both arrays and subtract each corresponding element from one another
 
 if __name__ == "__main__":
-    pass #do something
+    print(data)
+
+    #declaring arrays
+    leftColumnArray = []
+    rightColumnArray = []
+
+    #adding individual elements into their respective arrays
+    for item in data:
+        leftColumnElement = item[0:5]
+        rightColumnElement = item[8:]
+
+        leftColumnArray.append(leftColumnElement)
+        rightColumnArray.append(rightColumnElement)

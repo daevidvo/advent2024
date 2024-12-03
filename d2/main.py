62efc33deb436f095eb50e666b008050c10f4119
daevidvo <itsdavidvo@gmail.com>
2024-12-02 22:59:03 -0800

    cleaning up day2 part1 and adding comments for future use

--- d2/main.py
+++ d2/main.py
@@ -8,6 +8,7 @@ class Report:
         self.numberOfSafeReports = 0
         
     def splitLinesIntoArray(self):
+        # splitting each line into an array instead of having it be a string
         for i in range(0,len(self.data)):
             tempArray = self.data[i].rsplit(" ")
 
@@ -15,8 +16,8 @@ class Report:
 
     def checkForIncreasingNums(self):
         # turn each line into an array with the separation points being the spaces
-        # iterate through the array to check if the the next number is greater than the previous number
-        # if the next number isn't greater than previous, then set the flag to false.
+        # iterate through the array to check if the the next number is greater than the current number
+        # if the next number isn't greater than current, then set the flag to false.
 
         for i in range(0,len(self.data)):
             for j in range(0,len(self.data[i])-1):
@@ -25,13 +26,17 @@ class Report:
                     break
                 else:
                     self.flag = True 
-            
+
+            # if the flag is true, then proceed to go through the array and see if each of the numbers differ by more than 3 or less than 1 
             if self.flag == True:
                 self.checkForIncrementAmount(self.data[i])  
 
         return
 
     def checkForDecreasingNums(self):
+        # iterate through the array to check if the the next number is less than the current number
+        # if the next number isn't less than current, then set the flag to false.
+
         for i in range(0,len(self.data)):
             for j in range(0,len(self.data[i])-1):
                 if int(self.data[i][j]) < int(self.data[i][j+1]):
@@ -39,23 +44,24 @@ class Report:
                     break
                 else:
                     self.flag = True
-            
+
+            # if the flag is true, then proceed to go through the array and see if each of the numbers differ by more than 3 or less than 1 
             if self.flag == True:
                 self.checkForIncrementAmount(self.data[i]) 
         
         return
         
     def checkForIncrementAmount(self, array: dict):
-        print(array)
-
         for i in range(0,len(array)-1):
-            print(array[i])
+            # see if the current number and next numbers differ by less than 1 or more than 3
+            # if they the previous statement is true, then set flag to false
             if abs(int(array[i])-int(array[i+1])) < 1 or abs(int(array[i])-int(array[i+1])) > 3:
                 self.flag = False
                 break
             else:
                 self.flag = True
-        
+
+        # if flag is true, then increment the number of safe reports  
         if self.flag == True:
             self.numberOfSafeReports+=1
          
@@ -63,6 +69,7 @@ class Report:
 
     def returnNumberOfSafeReports(self):
         return self.numberOfSafeReports 
+
 if __name__ == "__main__":
     # part1
 

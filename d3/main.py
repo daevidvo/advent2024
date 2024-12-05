423aaa3ab1afc28becd7f7ddb321dfca893561ca
daevidvo <itsdavidvo@gmail.com>
2024-12-04 21:24:56 -0800

    got regex matching to work

--- d3/main.py
+++ d3/main.py
@@ -1,5 +1,32 @@
-data = open("./d3/d3data.txt","r").read().splitlines()
+import re
+
+data = open("./d3/d3data.txt","r").read().split("mul(")
+
+class Stack:
+    def __init__(self, data:dict ):
+        self.stack = data 
+    
+    def append(self, data: str) -> dict:
+        return self.stack.append()
+    
+    def pop(self) -> str:
+        return self.stack.pop()
+    
+
+
 
 if __name__ == "__main__":
-    # do something
+    # part1
+    # Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
+
+    mulStack = Stack(data)
+
+    regex = re.compile("^\d{1,3},\d{1,3}\)")
+
+    for item in mulStack.stack:
+        if re.match(regex,item):
+            tempString = item.split(")")
+
+            
+            
     pass
